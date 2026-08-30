d2aed11e0f2f2c720d664cd92647a33d15436b64
Tom Golding <thomas.r.golding@outlook.com>
2026-08-31 08:28:56 +1000

    refactor(magic): added unused variation for future use

--- backend/src/domain/chess/utils/magic.utils.ts
+++ backend/src/domain/chess/utils/magic.utils.ts
@@ -1,15 +1,20 @@
 import { BOARD_MASK } from "../board/board.constants.js";
 import { Bitboard } from "../board/types.chess.js";
+import crypto from "crypto";
 
 export function computeIndex(
   blockerMask: Bitboard,
   magic: Bitboard,
   shift: number,
 ): number {
-  return Number(((blockerMask * magic) & BOARD_MASK) >> BigInt(shift));
+  blockerMask *= magic;
+  blockerMask &= BOARD_MASK;
+  blockerMask >>= BigInt(shift);
+  return Number(blockerMask);
 }
 
 function random64(): bigint {
+  crypto.randomBytes;
   return (
     (BigInt(Math.floor(Math.random() * 0x100000000)) << 32n) |
     BigInt(Math.floor(Math.random() * 0x100000000))
@@ -17,4 +22,15 @@ function random64(): bigint {
 }
 export function randomMagic(): bigint {
   return random64() & random64() & random64();
+}
+export function randomBigint() {
+  let u1: bigint, u2: bigint, u3: bigint, u4: bigint;
+  u1 = random64() & 0xffffn;
+  u2 = random64() & 0xffffn;
+  u3 = random64() & 0xffffn;
+  u4 = random64() & 0xffffn;
+  return u1 | (u2 << 16n) | (u3 << 32n) | (u4 << 48n);
+}
+export function randomSparseBigint() {
+  return randomBigint() & randomBigint() & randomBigint();
 }
