899ccecb014e567c7bab5a789bb71c334a080331
Tom Golding <thomas.r.golding@outlook.com>
2026-08-31 08:08:59 +1000

    refactor(utils): reorganized file utils

--- backend/src/domain/chess/utils/file.utils.ts
+++ backend/src/domain/chess/utils/file.utils.ts
@@ -1,44 +1,14 @@
 import fs from "fs";
-export function saveGenerated(
-  filePath: string,
-  variableName: string,
-  values: (number | bigint)[],
-) {
-  const content = `
-export const ${variableName} = [
-${values.map((v) => `  0x${v.toString(16)}n,`).join("\n")}
-] as const;
-`;
 
-  fs.writeFileSync(filePath, content);
-}
-export function appendGenerated(
-  filePath: string,
-  variableName: string,
-  values: (number | bigint)[],
-) {
-  const content = `
-export const ${variableName} = [
-${values.map((v) => formatValue(v)).join("\n")}
-] as const;
-`;
-
+export function appendToFile(filePath: string, content: string): void {
   fs.appendFileSync(filePath, content);
 }
-function formatValue(value: number | bigint): string {
-  if (typeof value === "number") {
-    return `${value.toString()},`;
-  } else {
-    return `  0x${value.toString(16)}n,`;
-  }
+export function writeToFile(filePath: string, content: string): void {
+  fs.writeFileSync(filePath, content);
 }
-export function removeFile(filePath: string): void {
+export function deleteFile(filePath: string): void {
   fs.rmSync(filePath);
 }
 export function fileExists(filePath: string): boolean {
   return fs.existsSync(filePath);
-}
-
-export function clearFile(filePath: string) {
-  fs.rmSync(filePath);
 }
