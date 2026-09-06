d52be6f338b1508a5a5e6bae630514574c5c5dfb
Tom Golding <thomas.r.golding@outlook.com>
2026-09-07 08:12:29 +1000

    refactor(move): moved flags from types to constants

--- backend/src/domain/chess/movegen/move/move.constants.ts
+++ backend/src/domain/chess/movegen/move/move.constants.ts
@@ -10,3 +10,24 @@ export const MOVE_FROM_MASK = (1 << MOVE_FROM_BITS) - 1;
 export const MOVE_TO_MASK = MOVE_FROM_MASK << MOVE_TO_SHIFT;
 export const MOVE_FLAG_MASK = ((1 << MOVE_FLAG_BITS) - 1) << MOVE_FLAG_SHIFT;
 export const MOVE_MASK = (1 << (MOVE_FLAG_SHIFT + MOVE_FLAG_BITS)) - 1;
+
+export const MOVE_FLAGS = {
+  Quiet: 0,
+  DoublePawnPush: 1,
+  KingCastle: 2,
+  QueenCastle: 3,
+  Capture: 4,
+  EnPassantCapture: 5,
+  KnightPromotion: 6,
+  BishopCapture: 7,
+  RookPromotion: 8,
+  QueenPromotion: 9,
+  KnightPromotionCapture: 10,
+  BishopCaptureCapture: 11,
+  RookPromotionCapture: 12,
+  QueenPromotionCapture: 13,
+  Promotion: 14,
+  PromotionCapture: 15,
+  Check: 16,
+} as const;
+export type MoveFlag = (typeof MOVE_FLAGS)[keyof typeof MOVE_FLAGS];
--- backend/src/domain/chess/movegen/move/move.types.ts
+++ backend/src/domain/chess/movegen/move/move.types.ts
@@ -1,24 +1,3 @@
 export type Bitpack = number;
 
 export type Move = number & { readonly __brand: "Move" };
-
-export const MOVE_FLAGS = {
-  Quiet: 0,
-  DoublePawnPush: 1,
-  KingCastle: 2,
-  QueenCastle: 3,
-  Capture: 4,
-  EnPassantCapture: 5,
-  KnightPromotion: 6,
-  BishopCapture: 7,
-  RookPromotion: 8,
-  QueenPromotion: 9,
-  KnightPromotionCapture: 10,
-  BishopCaptureCapture: 11,
-  RookPromotionCapture: 12,
-  QueenPromotionCapture: 13,
-  Promotion: 14,
-  PromotionCapture: 15,
-  Check: 16,
-} as const;
-export type MoveFlag = (typeof MOVE_FLAGS)[keyof typeof MOVE_FLAGS];
