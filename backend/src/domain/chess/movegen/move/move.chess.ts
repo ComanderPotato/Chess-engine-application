fa9884742ee25fbe09a638eb6f0c0419054e9fee
Tom Golding <thomas.r.golding@outlook.com>
2026-09-10 16:34:15 +1000

    refactor(move): put functions previously in move.utils into created move.chess

---
+++ backend/src/domain/chess/movegen/move/move.chess.ts
@@ -0,0 +1,84 @@
+import { Square } from "../../board/types.chess.js";
+import { isValidSquare } from "../../utils/square.utils.js";
+import {
+  MoveFlag,
+  MOVE_TO_SHIFT,
+  MOVE_FLAG_SHIFT,
+  MOVE_MASK,
+  MOVE_FROM_MASK,
+  MOVE_TO_MASK,
+  MOVE_FLAG_MASK,
+  MOVE_FLAGS,
+} from "./move.constants.js";
+import { Move } from "./move.types.js";
+
+// Cast
+export function toMove(value: number): Move {
+  if (!isValidMove(value)) {
+    throw new Error("Invalid move");
+  }
+
+  return value as Move;
+}
+
+// Creation
+export function createMove(from: Square, to: Square, flag: MoveFlag): Move {
+  return (from | (to << MOVE_TO_SHIFT) | (flag << MOVE_FLAG_SHIFT)) as Move;
+}
+
+// Validation
+export function isValidMove(value: number): value is Move {
+  if (!Number.isInteger(value)) return false;
+  if (value < 0) return false;
+  if (value > MOVE_MASK) return false;
+
+  return hasValidFrom(value) && hasValidTo(value) && hasValidFlag(value);
+}
+
+export function hasValidFrom(value: number): boolean {
+  return isValidSquare(extractFrom(value));
+}
+export function hasValidTo(value: number): boolean {
+  return isValidSquare(extractTo(value));
+}
+export function hasValidFlag(value: number): boolean {
+  return isValidFlag(extractFlag(value));
+}
+
+// insertion
+export function setFrom(move: Move, from: Square): Move {
+  return ((move & ~MOVE_FROM_MASK) | from) as Move;
+}
+
+export function setTo(move: Move, to: Square): Move {
+  return ((move & ~MOVE_TO_MASK) | (to << MOVE_TO_SHIFT)) as Move;
+}
+
+export function setFlag(move: Move, flag: MoveFlag): Move {
+  return ((move & ~MOVE_FLAG_MASK) | (flag << MOVE_FLAG_SHIFT)) as Move;
+}
+
+// Validated extraction
+export function getFrom(move: Move): Square {
+  return extractFrom(move) as Square;
+}
+export function getTo(move: Move): Square {
+  return extractTo(move) as Square;
+}
+export function getFlag(move: Move): MoveFlag {
+  return extractFlag(move) as MoveFlag;
+}
+
+// Unvalidated extraction
+function extractFrom(value: number): number {
+  return value & MOVE_FROM_MASK;
+}
+function extractTo(value: number): number {
+  return (value & MOVE_TO_MASK) >> MOVE_TO_SHIFT;
+}
+function extractFlag(value: number): number {
+  return (value & MOVE_FLAG_MASK) >> MOVE_FLAG_SHIFT;
+}
+export function isValidFlag(value: number): value is MoveFlag {
+  return value >= MOVE_FLAGS.Quiet && value <= MOVE_FLAGS.Check;
+}
--- backend/src/domain/chess/utils/move.utils.ts
+++ backend/src/domain/chess/utils/move.utils.ts
@@ -1,82 +1,46 @@
-import { Square } from "../board/types.chess.js";
 import {
-  MOVE_FLAG_SHIFT,
-  MOVE_FROM_MASK,
-  MOVE_MASK,
-  MOVE_TO_MASK,
-  MOVE_TO_SHIFT,
-} from "../movegen/move/move.constants.js";
-import { MOVE_FLAG_MASK } from "../movegen/move/move.constants.js";
-import { Move, MOVE_FLAGS, MoveFlag } from "../movegen/move/move.types.js";
-import { isValidSquare } from "./square.utils.js";
+  STARTING_BLACK_PAWNS_MASK,
+  STARTING_WHITE_PAWNS_MASK,
+} from "../bitboard/bitboard.masks.js";
+import { Board } from "../board/board.chess.js";
+import { RANK_MASKS, RANKS } from "../board/board.constants.js";
+import { Bitboard, Square } from "../board/types.chess.js";
+import { shiftLeft, shiftRight } from "../movegen/move/move.bitboard.js";
+import { MOVE_FLAGS, MoveFlag } from "../movegen/move/move.constants.js";
+import { BITBOARD_SHIFTS } from "../movegen/move/move.directions.js";
+import { getType, isWhite } from "../piece/piece.chess.js";
+import { PIECE_TYPES } from "../piece/piece.constants.js";
+import { Piece, PieceColour } from "../piece/piece.types.js";
 
-// Cast
-export function toMove(value: number): Move {
-  if (!isValidMove(value)) {
-    throw new Error("Invalid move");
-  }
-
-  return value as Move;
-}
-
-// Creation
-export function createMove(from: Square, to: Square, flag: MoveFlag): Move {
-  return (from | (to << MOVE_TO_SHIFT) | (flag << MOVE_FLAG_SHIFT)) as Move;
+export function isPromotion(to: Square): boolean {
+  const toMask = 1n << BigInt(to);
+  return (
+    (toMask & RANK_MASKS[RANKS.EIGHTH]!) !== 0n ||
+    (toMask & RANK_MASKS[RANKS.FIRST]!) !== 0n
+  );
 }
 
-// Validation
-export function isValidMove(value: number): value is Move {
-  if (!Number.isInteger(value)) return false;
-  if (value < 0) return false;
-  if (value > MOVE_MASK) return false;
+export function assignFlag(board: Board, from: Square, to: Square): MoveFlag {
+  const toSquareOccupancy = board.pieceAt(to);
 
-  return hasValidFrom(value) && hasValidTo(value) && hasValidFlag(value);
+  return MOVE_FLAGS.Quiet;
 }
 
-export function hasValidFrom(value: number): boolean {
-  return isValidSquare(extractFrom(value));
-}
-export function hasValidTo(value: number): boolean {
-  return isValidSquare(extractTo(value));
-}
-export function hasValidFlag(value: number): boolean {
-  return isValidFlag(extractFlag(value));
-}
+export function generatePawnMoveBoard(
+  board: Board,
+  colour: PieceColour,
+): Bitboard {
+  const pawnBoard = board.getBitboard(PIECE_TYPES.Pawn | colour);
+  const shiftDirection = isWhite(colour) ? shiftRight : shiftLeft;
+  const startingRank = isWhite(colour)
+    ? STARTING_WHITE_PAWNS_MASK
+    : STARTING_BLACK_PAWNS_MASK;
 
-// insertion
-export function setFrom(move: Move, from: Square): Move {
-  return ((move & ~MOVE_FROM_MASK) | from) as Move;
-}
-
-export function setTo(move: Move, to: Square): Move {
-  return ((move & ~MOVE_TO_MASK) | (to << MOVE_TO_SHIFT)) as Move;
-}
-
-export function setFlag(move: Move, flag: MoveFlag): Move {
-  return ((move & ~MOVE_FLAG_MASK) | (flag << MOVE_FLAG_SHIFT)) as Move;
-}
-
-// Validated extraction
-export function getFrom(move: Move): Square {
-  return extractFrom(move) as Square;
-}
-export function getTo(move: Move): Square {
-  return extractTo(move) as Square;
-}
-export function getFlag(move: Move): MoveFlag {
-  return extractFlag(move) as MoveFlag;
-}
-
-// Unvalidated extraction
-function extractFrom(value: number): number {
-  return value & MOVE_FROM_MASK;
-}
-function extractTo(value: number): number {
-  return (value & MOVE_TO_MASK) >> MOVE_TO_SHIFT;
-}
-function extractFlag(value: number): number {
-  return (value & MOVE_FLAG_MASK) >> MOVE_FLAG_SHIFT;
-}
-export function isValidFlag(value: number): value is MoveFlag {
-  return value >= MOVE_FLAGS.Quiet && value <= MOVE_FLAGS.Check;
+  const singlePush = shiftDirection(pawnBoard, 8, ~board.occupancy);
+  const doublePush = shiftDirection(
+    singlePush & shiftDirection(startingRank, 8),
+    8,
+    ~board.occupancy,
+  );
+  return singlePush & doublePush;
 }
