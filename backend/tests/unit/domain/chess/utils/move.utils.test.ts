c2a600f1994b684151638214d0286629218b75be
Tom Golding <thomas.r.golding@outlook.com>
2026-09-11 16:13:47 +1000

    WIP(move): added scaffold tests for move util functions

--- backend/tests/unit/domain/chess/utils/move.utils.test.ts
+++ backend/tests/unit/domain/chess/utils/move.utils.test.ts
@@ -1,127 +1,41 @@
-import { TEST_CATEGORIES } from "#/utils/test-constants.js";
-import { beforeEach, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import * as TestMoveUtils from "@/domain/chess/utils/move.utils.js";
+import { TEST_CATEGORIES } from "#/utils/test-constants.js";
+import { EXPECTED_RANKS } from "#/fixtures/chess/square.fixtures.js";
+import { RANKS, SQUARES } from "@/domain/chess/board/board.constants.js";
+import { Board } from "@/domain/chess/board/board.chess.js";
 import {
-  Move,
-  MOVE_FLAGS,
-  MoveFlag,
-} from "@/domain/chess/movegen/move/move.types.js";
-import { Square } from "@/domain/chess/board/types.chess.js";
-import { SQUARES } from "@/domain/chess/board/board.constants.js";
-import { MOVE_MASK } from "@/domain/chess/movegen/move/move.constants.js";
+  printBitboard,
+  printChessBoard,
+} from "@/domain/chess/debug/printer.chess.js";
 
 describe("move.utils", () => {
-  let move: Move;
-  let from: Square;
-  let to: Square;
-  let flag: MoveFlag;
-  beforeEach(() => {
-    from = 0 as Square;
-    to = 10 as Square;
-    flag = MOVE_FLAGS.Quiet;
-    move = TestMoveUtils.createMove(from, to, flag);
-  });
-  describe(TEST_CATEGORIES.CREATION, () => {
-    it("creates a valid move", () => {
-      expect(Number.isInteger(move)).toBe(true);
-      expect(move).toBeGreaterThanOrEqual(0);
-      expect(move).toBeLessThanOrEqual(MOVE_MASK);
+  describe(TEST_CATEGORIES.UNCATEGORISED, () => {
+    describe(TestMoveUtils.isOccupied, () => {
+      it("returns true when a to square", () => {});
     });
   });
-  describe(TEST_CATEGORIES.EXTRACTION, () => {
-    describe(TestMoveUtils.getFrom.name, () => {
-      it("gets the from square from the move", () => {
-        expect(TestMoveUtils.getFrom(move)).toBe(from);
-      });
-    });
-    describe(TestMoveUtils.getTo.name, () => {
-      it("gets the to square from the move", () => {
-        expect(TestMoveUtils.getTo(move)).toBe(to);
-      });
-    });
-    describe(TestMoveUtils.getFlag.name, () => {
-      it("gets the flag from the move", () => {
-        expect(TestMoveUtils.getFlag(move)).toBe(flag);
-      });
-    });
-  });
-  describe(TEST_CATEGORIES.INSERTION, () => {
-    describe(TestMoveUtils.setFrom.name, () => {
-      it("sets the from square of a move", () => {
-        let prev = from;
+  describe(TEST_CATEGORIES.CLASSIFICATION, () => {
+    describe(TestMoveUtils.isPawnPromotion.name, () => {
+      it("returns true only a move to the first or eighth rank", () => {
         for (const square of SQUARES) {
-          expect(TestMoveUtils.getFrom(move)).toBe(prev);
-          prev = square;
-          move = TestMoveUtils.setFrom(move, square);
-          expect(TestMoveUtils.getFrom(move)).toBe(square);
+          expect(TestMoveUtils.isPawnPromotion(square)).toBe(
+            EXPECTED_RANKS[square] === RANKS.FIRST ||
+              EXPECTED_RANKS[square] === RANKS.EIGHTH,
+          );
         }
       });
     });
-    describe(TestMoveUtils.setTo.name, () => {
-      it("sets the to square of a move", () => {
-        let prev = to;
-        for (const square of SQUARES) {
-          expect(TestMoveUtils.getTo(move)).toBe(prev);
-          prev = square;
-          move = TestMoveUtils.setTo(move, square);
-          expect(TestMoveUtils.getTo(move)).toBe(square);
-        }
-      });
-    });
-    describe(TestMoveUtils.setFlag.name, () => {
-      it("sets the flag of a move", () => {
-        let prev = flag;
-        for (const newFlag of Object.values(MOVE_FLAGS)) {
-          expect(TestMoveUtils.getFlag(move)).toBe(prev);
-          prev = newFlag;
-          move = TestMoveUtils.setFlag(move, newFlag);
-          expect(TestMoveUtils.getFlag(move)).toBe(newFlag);
-        }
-      });
-    });
-  });
-  describe(TEST_CATEGORIES.VALIDATION, () => {
-    describe(TestMoveUtils.isValidMove.name, () => {
-      it("returns true when a move is valid", () => {
-        expect(TestMoveUtils.isValidMove(move)).toBe(true);
-      });
-      it("returns false with invalid input", () => {
-        expect(TestMoveUtils.isValidMove(-1)).toBe(false);
-        expect(TestMoveUtils.isValidMove(MOVE_MASK << 1)).toBe(false);
-      });
-    });
-    describe(TestMoveUtils.hasValidFrom.name, () => {
-      it("returns true for a moves valid from", () => {
-        expect(TestMoveUtils.hasValidFrom(move)).toBe(true);
-      });
-    });
-    describe(TestMoveUtils.hasValidTo.name, () => {
-      it("returns true for a moves valid to", () => {
-        expect(TestMoveUtils.hasValidTo(move)).toBe(true);
-      });
-    });
-    describe(TestMoveUtils.hasValidFlag.name, () => {
-      it("returns true for a moves valid flag", () => {
-        expect(TestMoveUtils.hasValidFlag(move)).toBe(true);
+    describe(TestMoveUtils.generatePawnMoveBoard.name, () => {
+      it("test", () => {
+        const board = new Board();
+        const a = TestMoveUtils.generatePawnMoveBoard(
+          board,
+          board.activeColour,
+        );
+        printChessBoard(board);
+        printBitboard(a);
       });
     });
   });
 });
-// describe(TEST_CATEGORIES.CREATION, () => {
-//   describe(TestMoveUtils.createMove.name, () => {
-//     it("creates a valid move", () => {
-//       for (const from of SQUARES) {
-//         for (const to of SQUARES) {
-//           for (const flag of Object.values(MOVE_FLAGS)) {
-//             const move = TestMoveUtils.createMove(from, to, flag);
-//
-//             expect(TestMoveUtils.getFrom(move)).toBe(from);
-//             expect(TestMoveUtils.getTo(move)).toBe(to);
-//             expect(TestMoveUtils.getFlag(move)).toBe(flag);
-//             expect(move & ~MOVE_MASK).toBe(0);
-//           }
-//         }
-//       }
-//     });
-//   });
-// });
