5ffdebfa77d109d5a3ca2461bb90e799c15f0aaa
Tom Golding <thomas.r.golding@outlook.com>
2026-09-11 16:12:32 +1000

    WIP(move): working on move util functions - mainly flag related

--- backend/src/domain/chess/utils/move.utils.ts
+++ backend/src/domain/chess/utils/move.utils.ts
@@ -6,13 +6,26 @@ import { Board } from "../board/board.chess.js";
 import { RANK_MASKS, RANKS } from "../board/board.constants.js";
 import { Bitboard, Square } from "../board/types.chess.js";
 import { shiftLeft, shiftRight } from "../movegen/move/move.bitboard.js";
-import { MOVE_FLAGS, MoveFlag } from "../movegen/move/move.constants.js";
-import { BITBOARD_SHIFTS } from "../movegen/move/move.directions.js";
-import { getType, isWhite } from "../piece/piece.chess.js";
-import { PIECE_TYPES } from "../piece/piece.constants.js";
+import { createMove } from "../movegen/move/move.chess.js";
+import {
+  MOVE_FLAGS,
+  MoveFlag,
+  PROMOTION_CAPTURE_FLAGS,
+  PROMOTION_FLAGS,
+} from "../movegen/move/move.constants.js";
+import {
+  ALL_DIRECTIONS,
+  BITBOARD_SHIFTS,
+  SQUARE_DIRECTIONS,
+} from "../movegen/move/move.directions.js";
+import { Move } from "../movegen/move/move.types.js";
+import { getColour, getType, isWhite } from "../piece/piece.chess.js";
+import { PIECE_COLOURS, PIECE_TYPES } from "../piece/piece.constants.js";
 import { Piece, PieceColour } from "../piece/piece.types.js";
+import { isBitSet64 } from "./bit.utils.js";
+import { toRank, toSquare } from "./square.utils.js";
 
-export function isPromotion(to: Square): boolean {
+export function isPawnPromotion(to: Square): boolean {
   const toMask = 1n << BigInt(to);
   return (
     (toMask & RANK_MASKS[RANKS.EIGHTH]!) !== 0n ||
@@ -20,10 +33,66 @@ export function isPromotion(to: Square): boolean {
   );
 }
 
-export function assignFlag(board: Board, from: Square, to: Square): MoveFlag {
-  const toSquareOccupancy = board.pieceAt(to);
+export function isOccupied(board: Board, to: Square): boolean {
+  return isBitSet64(board.occupancy, to);
+}
 
-  return MOVE_FLAGS.Quiet;
+export function isEnPassantCapture(
+  board: Board,
+  from: Square,
+  to: Square,
+): boolean {
+  return toRank(from) !== toRank(to) && !isOccupied(board, to);
+}
+
+export function generateMoves(
+  board: Board,
+  from: Square,
+  to: Square,
+  moves: Move[],
+): Move[] {
+  const isCapture = isOccupied(board, to);
+  const piece = board.pieceAt(from);
+  if (getType(piece) === PIECE_TYPES.Pawn) {
+    if (isPawnPromotion(to)) {
+      const flags = isCapture ? PROMOTION_CAPTURE_FLAGS : PROMOTION_FLAGS;
+      for (const flag of flags) {
+        moves.push(createMove(from, to, flag));
+      }
+    } else if (isEnPassantCapture(board, from, to)) {
+      moves.push(createMove(from, to, MOVE_FLAGS.EnPassantCapture));
+    } else if (isCapture) {
+      moves.push(createMove(from, to, MOVE_FLAGS.Capture));
+    } else {
+      moves.push(createMove(from, to, MOVE_FLAGS.Quiet));
+    }
+  } else if (getType(piece) === PIECE_TYPES.King) {
+    const rooks = board.bitboardsList[PIECE_TYPES.Rook | getColour(piece)]!;
+    const hasRookToWest = isBitSet64(rooks, toSquare(to + SQUARE_DIRECTIONS.W));
+    const hasRookToEast = isBitSet64(rooks, toSquare(to + SQUARE_DIRECTIONS.E));
+    const [hasKingSide, hasQueenSide] =
+      getColour(piece) === PIECE_COLOURS.White
+        ? [
+            board.getWhiteKingSideCastlingRights(),
+            board.getWhiteQueenSideCastlingRights(),
+          ]
+        : [
+            board.getBlackKingSideCastlingRights(),
+            board.getBlackQueenSideCastlingRights(),
+          ];
+    if (hasRookToWest && hasQueenSide) {
+      moves.push(createMove(from, to, MOVE_FLAGS.QueenCastle));
+    } else if (hasRookToEast && hasKingSide) {
+      moves.push(createMove(from, to, MOVE_FLAGS.KingCastle));
+    }
+  } else {
+    if (isCapture) {
+      moves.push(createMove(from, to, MOVE_FLAGS.Capture));
+    } else {
+      moves.push(createMove(from, to, MOVE_FLAGS.Quiet));
+    }
+  }
+  return moves;
 }
 
 export function generatePawnMoveBoard(
